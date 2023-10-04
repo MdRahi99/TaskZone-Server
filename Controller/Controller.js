4c0c61f9e7ce772d06623774dc6f6d6fde13f0e9
MD Rahi <fhrstation21@gmail.com>
2023-10-04 18:19:17 +0600

    update route create successfully

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -4,37 +4,75 @@ exports.Home = (req, res) => {
     res.send('TaskZone Server Running...')
 };
 
-exports.PostTasks = async(req, res) => {
-    try{
-        const {text} = req.body;
-        const tasks = new Tasks({text});
+exports.PostTasks = async (req, res) => {
+    try {
+        const { text } = req.body;
+        const tasks = new Tasks({ text });
         await tasks.save();
         res.status(201).json(tasks);
-    } 
-    catch(error){
-        res.status(500).json({error: 'Internal Server Error'})
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' })
     }
 };
 
-exports.GetTasks = async(req, res) => {
-    try{
+exports.GetTasks = async (req, res) => {
+    try {
         const tasks = await Tasks.find();
         res.json(tasks);
-    } 
-    catch(error){
-        res.status(500).json({error: 'Internal Server Error'})
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' })
     }
 };
-exports.DeleteTask = async(req, res) => {
-    try{
-        const {_id} = req.params;
+
+exports.GetTask = async (req, res, next) => {
+    try {
+        const taskId = req.params.id;
+        const task = await Tasks.findById(taskId);
+
+        if (!task) {
+            return res.status(404).json({ error: 'Task not found' });
+        }
+
+        res.status(200).json(task);
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.DeleteTask = async (req, res) => {
+    try {
+        const { _id } = req.params;
         const deletedTask = await Tasks.deleteOne(_id);
-        if(!deletedTask){
+        if (!deletedTask) {
             return res.status(404).json({ message: 'Task not found' });
         }
-        res.status(200).json({message: 'Task Deleted Successfully'});
+        res.status(200).json({ message: 'Task Deleted Successfully' });
     }
-    catch(error){
-        res.status(500).json({error: 'Internal Server Error'})
+    catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' })
     }
 };
+
+exports.UpdateTask = async (req, res) => {
+    const id = req.params.id;
+    const updatedTaskData = req.body;
+  
+    try {
+      const updatedTask = await Tasks.findOneAndUpdate(
+        {_id: id},
+        updatedTaskData,
+        { new: true }
+      );
+  
+      if (!updatedTask) {
+        return res.status(404).json({ message: 'Task not found' });
+      }
+  
+      return res.json(updatedTask);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal Server error' });
+    }
+  };
--- Routes/Routes.js
+++ Routes/Routes.js
@@ -4,7 +4,9 @@ const Controller = require('../Controller/Controller');
 
 router.get('/', Controller.Home);
 router.get('/tasks', Controller.GetTasks);
+router.get('/task/:id', Controller.GetTask);
 router.post('/tasks', Controller.PostTasks);
 router.delete('/task/:id', Controller.DeleteTask);
+router.put('/task/:id', Controller.UpdateTask);
 
 module.exports = router;
